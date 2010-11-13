08e33a5395193dc6598c9102bc297e8adfa5cd77
Erik Hollensbe <erik@hollensbe.org>
2010-11-13 01:47:12 -0500

    * Renamed test file

--- test/test_rubygems-test.rb
+++ test/test_rubygems-test.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class TestBasicMethods < Test::Unit::TestCase
+class TestRubyGemsTest < Test::Unit::TestCase
   def test_01_successfully_uninstalls_on_test_failure
     set_configuration({ "force_uninstall_on_failure" => true, "auto_test_on_install" => true })
     puts install_stub_gem(:test_files => [])
